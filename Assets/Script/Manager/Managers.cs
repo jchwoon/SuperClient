542f3b7bb4d904ed2c7f1ff0353d0169cb9f2bf0
정채운 <toyboy01113@gmail.com>
2024-08-19 12:05:53 +0900

    feat (ServerCore) : transfer core

--- Assets/Script/Manager/Managers.cs
+++ Assets/Script/Manager/Managers.cs
@@ -1,16 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Resources;
+using UnityEditor.EditorTools;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Managers : MonoBehaviour
 {
-    private void Start()
+    private static Managers s_instance;
+
+    private static Managers Instance
     {
-        
+        get { MainInit(); return s_instance; }
     }
 
-    private void Update()
+    private NetworkManager _networkManager = new NetworkManager();
+
+    public static NetworkManager NetworkManager { get { return Instance._networkManager; } }
+
+    private void Awake()
     {
-        
+        MainInit();
+    }
+    private static void MainInit()
+    {
+        if (s_instance == null)
+        {
+            GameObject gameObject = GameObject.Find("Managers");
+            if (gameObject == null)
+            {
+                gameObject = new GameObject("Managers");
+                gameObject.AddComponent<Managers>();
+            }
+            DontDestroyOnLoad(gameObject);
+
+            s_instance = gameObject.GetComponent<Managers>();
+        }
     }
 }
